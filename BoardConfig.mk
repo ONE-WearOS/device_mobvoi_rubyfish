103f4b542302799735a4116753b1ce7acfd2cce1
BadWolf-Alfa <marco@lunawolf.es>
2021-10-02 14:56:12 +0200

    Reworked, now works all asap

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,43 +1,22 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+PLATFORM_PATH := device/mobvoi/rubyfish
 
-DEVICE_PATH := device/mobvoi/rover
+LC_ALL=C
+ALLOW_MISSING_DEPENDENCIES=true
 
-# For building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := msm8937
 TARGET_NO_BOOTLOADER := true
-
-# Platform
+TARGET_BOOTLOADER_BOARD_NAME := MSM8937
 TARGET_BOARD_PLATFORM := msm8937
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno504
 
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_ABI := armeabi
+TARGET_CPU_VARIANT := cortex-a15
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := rubyfish
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8937
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -49,65 +28,78 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
-# System-as-root
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-
+# Cmdline
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true androidboot.selinux=permissive androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image  loop.max_part=7 
+BOARD_KERNEL_CMDLINE += android.boot.selinux=permissive
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=rubyfish msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1
-#BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=rubyfish user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7 buildvariant=user
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage-dtb
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_INCLUDE_RECOVERY_DTBO := true
-BOARD_BOOTIMG_HEADER_VERSION := 1
-BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_KERNEL_SOURCE := kernel/mobvoi/rover
-TARGET_KERNEL_CONFIG := rubyfish_defconfig
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_KERNEL_BASE := 0x80000000
+TARGET_PREBUILT_KERNEL := $(PLATFORM_PATH)/prebuilt/zImage-dtb
 
-# Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
-# Decrypt for now no work....
-#TW_INCLUDE_CRYPTO := true
-#TARGET_HW_DISK_ENCRYPTION := true
-#TARGET_PROVIDES_KEYMASTER := true
-#TW_INCLUDE_CRYPTO_FBE := true
-#TW_INCLUDE_FBE_METADATA_DECRYPT := true
-#BOARD_USES_METADATA_PARTITION := true
+# Fstab
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
 
-# Android Verified Boot
-BOARD_AVB_ENABLE := false
-BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_rubyfish
+TARGET_RECOVERY_DEVICE_MODULES := libinit_rubyfish
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
-# TWRP Configuration
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_ROUND_SCREEN := true
-TW_THEME := watch_mdpi
-RECOVERY_SDCARD_ON_DATA := true
-# Qualcomm support
+# QCOM Stuff
 BOARD_USES_QCOM_HARDWARE := true
+
+# AVB
+#BOARD_AVB_ENABLE := true
+#BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+
+# Filesystem
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_METADATA_PARTITION := true
+
+# TWRP Stuff
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# TW_ROUND_SCREEN := true
+TW_THEME := watch_mdpi
+TW_USE_TOOLBOX := true
+HAVE_SELINUX := true
+RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_NO_LEGACY_PROPS := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
-TW_IGNORE_MISC_WIPE_DATA := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_USE_TOOLBOX := true
+TW_SCREEN_BLANK_ON_BOOT := false
 TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
 TW_HAS_EDL_MODE := true
 TW_EXCLUDE_TWRPAPP := true
+TARGET_USES_LOGD := true
+TW_NO_USB_STORAGE := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+
+
+# Show build time on the splash screen
+TW_DEVICE_VERSION=$(shell date '+%Y%m%d')
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2025-12-31
+
+# Platform version
+PLATFORM_VERSION := 16.1.0
--- omni_rubyfish.mk
+++ omni_rubyfish.mk
@@ -1,13 +1,11 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,22 +14,33 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-# $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Release name
 
-# Inherit from rover device
-# $(call inherit-product, device/mobvoi/rover/device.mk)
-
-# Inherit some common Omni stuff.
+# Inherit from this configs
+$(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
-# $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions.
+
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/prebuilt/kernel:kernel \
+#	$(LOCAL_PATH)/recovery.fstab:recovery.fstab
+
+# Device identifier. This must come after all inclusions.
+
 PRODUCT_DEVICE := rubyfish
 PRODUCT_NAME := omni_rubyfish
 PRODUCT_BRAND := Mobvoi
 PRODUCT_MODEL := TicWatch Pro 3
 PRODUCT_MANUFACTURER := mobvoi
 PRODUCT_RELEASE_NAME := Mobvoi TicWatch Pro 3
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.device \
+    ro.product.name \
+    ro.build.product
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2025-12-31 \
+	ro.secure=1 \
+	ro.adb.secure=0
